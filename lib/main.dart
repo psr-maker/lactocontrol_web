a81961de4ce74bc9f33bdc1abbdcc92cc6c9a0d9
psr-maker <randdpoornasreekeerthana@gmail.com>
2026-07-27 10:14:42 +0530

    abcd

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
-    //*-*-
   }
 }
